45a7186d4d5e7758440ccda9a212eeae656d17df
Moizés Júnior <128239325+MoizesJr@users.noreply.github.com>
2026-01-09 07:17:45 -0300

    feat: estrutura inicial do frontend unificada

---
+++ frontend/src/app/pages/produtos-lista/produtos-lista.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produtos-lista',
+  imports: [],
+  templateUrl: './produtos-lista.html',
+  styleUrl: './produtos-lista.scss',
+})
+export class ProdutosLista {
+
+}
